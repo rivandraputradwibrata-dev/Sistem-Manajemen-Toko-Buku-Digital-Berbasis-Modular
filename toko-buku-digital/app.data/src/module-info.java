aab8dde13837ff34596dccaf81a0c532e678a6b1
rivandraputradwibrata-dev <rivandraputradwibrata@gmail.com>
2026-06-24 14:18:17 +0700

    Delete toko-buku-digital directory

--- toko-buku-digital/app.data/src/module-info.java
+++
@@ -1,4 +0,0 @@
-module app.data {
-    // Mengekspor paket entitas data agar bisa digunakan modul lain
-    exports app.data;
-}
--- toko-buku-digital/app.logic/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module app.logic {
-    // Membutuhkan app.data untuk mengakses entitas dan repository
-    requires app.data;
-
-    // Mengekspor paket logika bisnis agar bisa digunakan oleh app.ui
-    exports app.logic;
-}
